1049d23f050ca565f1ade94c292620503072da40
patrickm96 <135382512+patrickm96@users.noreply.github.com>
2023-10-03 17:28:24 -0500

    Committing resources and js file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # belly-button-challenge
 Module 14 Challenge
+
+The following resources were referenced to complete this challenge:
+
+1. Javascript map array - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+2. ForEach array loop - https://www.w3schools.com/jsref/jsref_foreach.asp
+3. Slice and reverse array - https://stackoverflow.com/questions/30610523/reverse-an-array-in-javascript-without-mutating-the-original-array
+4. Create bubble chart using Plotly - https://plotly.com/javascript/bubble-charts/
+5. Create bar chart using Plotly - Instructor Eli covered this during class.
+6. Modify javascript code to be dynamic based on selected sample - ChatGPT
+7. Debugging dropdown menu population function - ChatGPT
---
+++ static/js/app.js
@@ -0,0 +1,138 @@
+// Get the endpoint
+const json_url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
+
+d3.json(json_url).then(function(dataResponse) {
+    data = dataResponse;
+    console.log(data);
+    console.log("Data Loaded");
+
+    init();
+    }
+);
+
+// Function to populate the dropdown options
+function populateDropdown(names) {
+    let dropdown = d3.select("#selDataset");
+    //dropdown.html("");
+    names.forEach(function(name) {
+    dropdown.append("option").text(name).property("value", name);
+    });
+};
+
+// Function to handle the dropdown change event
+function optionChanged(selectedValue) {
+    console.log("Selected value: " + selectedValue);
+    update_BarChart(selectedValue);
+    update_BubbleChart(selectedValue);
+    update_MetaData(selectedValue);
+};
+
+// Function to initialize the page
+function init() {
+    let names = data.names;
+    populateDropdown(names);
+    // Initial rendering of the chart
+    update_BarChart(names[0]);
+    update_BubbleChart(names[0]);
+    update_MetaData(names[0]);
+};
+
+// Function to update the bar chart based on the selected sample
+function update_BarChart(sampleID) {
+// Find the selected sample data
+    let samples = data.samples;
+    let selectedSample = samples.find(sample => sample.id === sampleID);
+// Sort the sample_values, otu_ids, and otu_labels in descending order
+    let sorted_samplevalues = selectedSample.sample_values.slice(0, 10).reverse();
+    let sorted_OTUids = selectedSample.otu_ids.slice(0, 10).reverse();
+    let sorted_OTUlabels = selectedSample.otu_labels.slice(0, 10).reverse();
+
+// Create the bar chart
+    let bar_trace = {
+        type: "bar",
+        x: sorted_samplevalues,
+        y: sorted_OTUids.map(id => `OTU ${id}`),
+        text: sorted_OTUlabels,
+        orientation: "h"
+    };
+
+    let bar_data = [bar_trace];
+
+    let bar_layout = {
+        title: `Top 10 OTUs for Sample ${sampleID}`,
+        yaxis: {title:"OTU ID"}
+    };
+
+    Plotly.newPlot("bar", bar_data, bar_layout);
+    console.log("Bar chart updated");
+};
+
+// Function to update the bubble chart based on the selected sample
+function update_BubbleChart(sampleID) {
+// Find the selected sample data
+    let samples = data.samples;
+    let selectedSample = samples.find(sample => sample.id === sampleID);
+
+    let OTUids = selectedSample.otu_ids;
+    let samplevalues = selectedSample.sample_values;
+    let OTUlabels = selectedSample.otu_labels;
+
+// Create the bubble chart
+    let bubble_trace = {
+        mode: "markers",
+        x: OTUids,
+        y: samplevalues,
+        text: OTUlabels,
+        marker: {
+          size: samplevalues,
+          color: OTUids
+        }
+    };
+
+    let bubble_data = [bubble_trace];
+
+    let bubble_layout = {
+        title: `Bubble Chart for Sample ${sampleID}`,
+        xaxis: { title:"OTU ID"}
+    };
+
+    Plotly.newPlot("bubble", bubble_data, bubble_layout);
+    console.log("Bubble chart updated");
+};
+
+// Function to update the Metadata based on the selected sample
+function update_MetaData(sampleID) {
+// Find the selected sample data
+    let samples = data.metadata;
+    let selectedSample = samples.find(sample => sample.id === parseInt(sampleID));
+
+    let Metadata_ID = selectedSample.id;
+    let Metadata_ethnicity = selectedSample.ethnicity;
+    let Metadata_gender = selectedSample.gender;
+    let Metadata_age = selectedSample.age;
+    let Metadata_location = selectedSample.location;
+    let Metadata_bbtype = selectedSample.bbtype;
+    let Metadata_wfreq = selectedSample.wfreq;
+
+    let Metadata_trace = {
+        "id": Metadata_ID,
+        "ethnicity": Metadata_ethnicity,
+        "gender": Metadata_gender,
+        "age": Metadata_age,
+        "location": Metadata_location,
+        "bbtype": Metadata_bbtype,
+        "wfreq": Metadata_wfreq
+    }
+
+    let Metadata_data = [Metadata_trace]
+  
+// Get the reference to the metadata div
+    let MetadataDiv = d3.select("#sample-metadata")
+    MetadataDiv.html("")
+// Append key value pairs to the metadata div
+    Object.entries(selectedSample).forEach(([key, value])=> {
+        MetadataDiv.append("p").text(key + ": " + value)
+    })
+    console.log("Metadata updated");
+};
+
